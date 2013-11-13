f750caa55aa75d32fc226310daf173266baf7068
Richard Woodbury <rpwoodbu@mybox.org>
2013-11-13 01:26:03 -0500

    Make tmux window persist on exit, aiding debugging.

--- start_mh.sh
+++ start_mh.sh
@@ -8,4 +8,6 @@ OPTS="-tk 0"
 mkdir -p /tmp/mh_data
 export mh_parms="${MH}/mh.private.ini"
 cd "${MH}/mh/bin"
-tmux new-session -s misterhouse "./mhl $OPTS"
+tmux new-session -d -s misterhouse "./mhl $OPTS"
+tmux set-window-option -t misterhouse remain-on-exit on
+tmux attach-session -t misterhouse
