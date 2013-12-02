313cdfb65473e0d43adc966d03318addac590b97
Richard Woodbury <rpwoodbu@mybox.org>
2013-12-02 00:38:13 -0500

    Fix triggers. Create and copy persistent_data. Remove generated files.
    persistent_data/ stores files that should be in the data/ directory, but might
    not be due to that directory being on tmpfs. This fixes triggers.

--- start_mh.sh
+++ start_mh.sh
@@ -6,6 +6,7 @@ MH="/home/rpwoodbu/code/misterhouse"
 OPTS="-tk 0"
 
 mkdir -p /tmp/mh_data
+ls persistent_data/* | grep -v 'README' | xargs -I FILES cp FILES data/
 export mh_parms="${MH}/mh.private.ini"
 cd "${MH}/mh/bin"
 tmux new-session -d -s misterhouse "./mhl $OPTS"
