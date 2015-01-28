6b52b1f99358c4c8a51ef342104193af56c74c26
Richard Woodbury <rpwoodbu@mybox.org>
2015-01-28 00:35:09 -0500

    Change MAC address for MythTV WoL.

--- code/do_mythtv.pl
+++ code/do_mythtv.pl
@@ -8,7 +8,7 @@ sub do_mythtv
     print_log "do_mythtv: Got a button press of " . $button_used->state;
     if ($button_used->state eq "on") {
         print_log "do_mythtv: Sending Wake-on-LAN to MythTV";
-        system("/usr/bin/wakeonlan 00:04:61:9d:74:2c");
+        system("/usr/bin/wakeonlan 1c:6f:65:93:35:fa");
         print_log "do_mythtv: Sending CEC to TV";
         my ($cmd) = "/bin/echo -ne \"p 0 1\\nas\\nq\\n\" | /usr/local/bin/cec-client";
         print_log $cmd;
